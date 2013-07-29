c67cc8b11dd343bbd6c1fa12b16c594d42ab8612
Kevin Patel <kevin36524@gmail.com>
2013-07-29 19:36:47 +0530

    Initial Commit

---
+++ Happiness/HappinessViewController.h
@@ -0,0 +1,13 @@
+//
+//  HappinessViewController.h
+//  Happiness
+//
+//  Created by Kevin Patel on 7/29/13.
+//  Copyright (c) 2013 Kevin Patel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HappinessViewController : UIViewController
+
+@end
---
+++ Happiness/HappinessViewController.m
@@ -0,0 +1,29 @@
+//
+//  HappinessViewController.m
+//  Happiness
+//
+//  Created by Kevin Patel on 7/29/13.
+//  Copyright (c) 2013 Kevin Patel. All rights reserved.
+//
+
+#import "HappinessViewController.h"
+
+@interface HappinessViewController ()
+
+@end
+
+@implementation HappinessViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
